997058256858fb8b55e9e8565ea6802f9c1b2e63
Josh Ribakoff <josh.ribakoff@gmail.com>
2015-06-19 04:06:42 -0700

    implement  dynamic tab creation, implement basic panels that toggle

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-angular.module('jrApp', ['jrPanel', 'jrContainer']);
+angular
+    .module('jrApp', ['jrPanel', 'jrContainer'])
+    .controller('demoController', function($scope) {
+        $scope.config = {
+            east: ['test 1', 'test 2'],
+            south: ['test 3', 'test 4'],
+            west: ['test 5', 'test 6']
+        }
+    });
--- src/container.js
+++ src/container.js
@@ -7,8 +7,55 @@ angular.module('jrContainer', [])
         function controller($scope, $element) {
             var containerElement = jQuery($element);
 
-            containerElement.find('.jrTabs').sortable({
-                connectWith: '.jrTabs'
+            containerElement.find('.jrTabs')
+                .sortable({
+                    connectWith: '.jrTabs'
+                });
+
+            $scope.$watch('jrContainerConfig', function(config) {
+                angular.forEach(config.east, function(name) {
+                    var newTab = jQuery("<li><a href=\"\">" + name + "</a></li>");
+                    containerElement.find('.jrEastTabs').append(newTab);
+                });
+                angular.forEach(config.west, function(name) {
+                    var newTab = jQuery("<li><a href=\"\">" + name + "</a></li>");
+                    containerElement.find('.jrWestTabs').append(newTab);
+                });
+                angular.forEach(config.south, function(name) {
+                    var newTab = jQuery("<li><a href=\"\">" + name + "</a></li>");
+                    containerElement.find('.jrSouthTabs').append(newTab);
+                });
+            });
+
+            containerElement.on('click', '.jrTabs a', function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                var clicked = jQuery(this);
+                var parentTabs = clicked.parents('.jrTabs');
+                var whichZone;
+                if(parentTabs.hasClass('jrSouthTabs')) {
+                    whichZone = 'South';
+                } else if(parentTabs.hasClass('jrWestTabs')) {
+                    whichZone = 'West';
+                } else if(parentTabs.hasClass('jrEastTabs')) {
+                    whichZone = 'East';
+                }
+                var panel = containerElement.find('.jrPanel' + whichZone);
+                panel.toggleClass('jrPanelOpen');
+                panel.html(clicked.text());
+                console.log('clicked a tab in zone',whichZone);
+
+                if('South' == whichZone) {
+                    var sPanel = containerElement.find('.jrPanelSouth');
+                    var sTabs = containerElement.find('.jrSouthTabs');
+                    var top;
+                    if(sPanel.hasClass('jrPanelOpen')) {
+                        top = (sPanel.height() + sTabs.height()) / containerElement.height();
+                    } else {
+                        top = sTabs.height() / containerElement.height();
+                    }
+                    containerElement.find('.jr-horizontal').css('bottom', top*100+'%');
+                }
             });
         }
 
@@ -20,7 +67,7 @@ angular.module('jrContainer', [])
             transclude: true,
             replace: true,
             scope: {
-                jrContainer: '='
+                jrContainerConfig: '='
             }
         }
     });
